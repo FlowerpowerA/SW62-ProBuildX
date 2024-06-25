b32a0f3a0dd6094db294f12a95d9209cc4d12469
CamilaLoli <u202110385@upc.edu.pe>
2024-06-24 22:01:19 -0500

    feat(resources-management): added the endpoints for the materials and machines in ProjectController.java

--- src/main/java/com/construtech/buildsphere/platform/project_management/interfaces/rest/ProjectController.java
+++ src/main/java/com/construtech/buildsphere/platform/project_management/interfaces/rest/ProjectController.java
@@ -12,6 +12,15 @@ import com.construtech.buildsphere.platform.project_management.interfaces.rest.r
 import com.construtech.buildsphere.platform.project_management.interfaces.rest.resources.ProjectResource;
 import com.construtech.buildsphere.platform.project_management.interfaces.rest.transform.CreateProjectCommandFromResourceAssembler;
 import com.construtech.buildsphere.platform.project_management.interfaces.rest.transform.ProjectResourceFromEntityAssembler;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.queries.GetAllMachinesByProjectIdQuery;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.queries.GetAllMaterialsByProjectIdQuery;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.valueobjects.Project;
+import com.construtech.buildsphere.platform.resourceManagement.domain.services.MachineQueryService;
+import com.construtech.buildsphere.platform.resourceManagement.domain.services.MaterialQueryService;
+import com.construtech.buildsphere.platform.resourceManagement.interfaces.rest.resources.MachineResource;
+import com.construtech.buildsphere.platform.resourceManagement.interfaces.rest.resources.MaterialResource;
+import com.construtech.buildsphere.platform.resourceManagement.interfaces.rest.transform.MachineResourceFromEntityAssembler;
+import com.construtech.buildsphere.platform.resourceManagement.interfaces.rest.transform.MaterialResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,10 +35,15 @@ import java.util.List;
 public class ProjectController {
     private final ProjectCommandService projectCommandService;
     private final ProjectQueryService projectQueryService;
+    private final MaterialQueryService materialQueryService;
+    private final MachineQueryService machineQueryService;
 
-    public ProjectController(ProjectCommandService projectCommandService, ProjectQueryService projectQueryService) {
+    public ProjectController(ProjectCommandService projectCommandService, ProjectQueryService projectQueryService, MaterialQueryService materialQueryService, MachineQueryService machineQueryService) {
         this.projectCommandService = projectCommandService;
         this.projectQueryService = projectQueryService;
+
+        this.materialQueryService = materialQueryService;
+        this.machineQueryService = machineQueryService;
     }
 
     @PostMapping
@@ -82,4 +96,25 @@ public class ProjectController {
         var projectResource = ProjectResourceFromEntityAssembler.toResourceFromEntity(dashboard.get().getProject());
         return ResponseEntity.ok(projectResource);
     }
+
+    @GetMapping("/{projectId}/materials")
+    public ResponseEntity<List<MaterialResource>> getAllMaterialsByProjectId(@PathVariable Long projectId) {
+        var project = new Project(projectId);
+        var getAllMaterialsByProjectIdQuery = new GetAllMaterialsByProjectIdQuery(project);
+        var materials = materialQueryService.handle(getAllMaterialsByProjectIdQuery);
+        var materialResources = materials.stream()
+                .map(MaterialResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(materialResources);
+    }
+
+    @GetMapping("/{projectId}/machines")
+    public ResponseEntity<List<MachineResource>> getAllMachinesByProjectId(@PathVariable Long projectId) {
+        var project = new Project(projectId);
+        var getAllMachinesByProjectIdQuery = new GetAllMachinesByProjectIdQuery(project);
+        var machines = machineQueryService.handle(getAllMachinesByProjectIdQuery);
+        var machineResources = machines.stream()
+                .map(MachineResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(machineResources);
+    }
+
 }

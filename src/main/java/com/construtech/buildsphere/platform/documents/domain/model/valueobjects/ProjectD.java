f5283068ca72d0824a6ba73fd28a9693610515b6
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-06-26 01:02:27 -0500

    feat(Document-Management, Inventory-Management): changed projectId to be implemented on ProjectController

--- src/main/java/com/construtech/buildsphere/platform/documents/domain/model/valueobjects/ProjectD.java
+++ src/main/java/com/construtech/buildsphere/platform/documents/domain/model/valueobjects/ProjectD.java
@@ -3,5 +3,5 @@ package com.construtech.buildsphere.platform.documents.domain.model.valueobjects
 import jakarta.persistence.Embeddable;
 
 @Embeddable
-public record Project(Long projectEnt) {
+public record ProjectD(Long projectEnt) {
 }
